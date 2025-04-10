090d1dd33deebe6cfaf52074b67138bac9fe657f
Beat Weichsler <beatweichsler@Beats-MacBook-Pro.local>
2025-04-10 16:36:07 +0200

    added all data files

--- src/load_neo4j_data.py
+++ src/load_neo4j_data.py
@@ -86,7 +86,7 @@ class ChemicalDatabase:
             logger.warning(f"Skipping chemical due to missing regulation: {chemical}")
             return
 
-        if chemical_name and cas and cas!="N/A":
+        if chemical_name and cas and cas!="N/A" and cas!="NA":
             query = """
             MERGE (c:Chemical {cas: $cas})
             MERGE (cn:ChemicalName {name: $name})
